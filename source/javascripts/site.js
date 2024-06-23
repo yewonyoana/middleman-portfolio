df96c5cafc481e4229f45305f5d9866913d2796c
Yoana Chang <yewonyoana@gmail.com>
2024-06-23 21:04:50 +0900

    Added JavaScript

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,15 @@
 // This is where it all goes :)
+const aboutDetail = document.getElementById("about-detail");
+
+function handleMouseOver(event) {
+  aboutDetail.innerText = "🇿🇦🇸🇮🇮🇩🇻🇳🇺🇸🇰🇷";
+  aboutDetail.style.textDecoration = "none";
+}
+
+function handleMouseLeave(event) {
+  aboutDetail.innerText = "six";
+  aboutDetail.style.textDecoration = "underline";
+}
+
+aboutDetail.addEventListener("mouseover", handleMouseOver);
+aboutDetail.addEventListener("mouseleave", handleMouseLeave);
