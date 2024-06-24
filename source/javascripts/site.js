1c599fa48c872726fd5c18020f659756118b42c0
Yoana Chang <yewonyoana@gmail.com>
2024-06-25 00:53:33 +0900

    Updated Navigation and Tech Stacks CSS

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,5 @@
 // This is where it all goes :)
+// About hover event
 const aboutDetail = document.getElementById("about-detail");
 
 function handleMouseOver(event) {
@@ -13,3 +14,30 @@ function handleMouseLeave(event) {
 
 aboutDetail.addEventListener("mouseover", handleMouseOver);
 aboutDetail.addEventListener("mouseleave", handleMouseLeave);
+
+// Tech stack click event
+// const backendBtn = document.getElementById("btn-backend");
+// const frontendBtn = document.getElementById("btn-frontend");
+// const othersBtn = document.getElementById("btn-others");
+// const techstackBackend = document.getElementById("techstack-backend");
+// const techstackFrontend = document.getElementById("techstack-frontend");
+// const techstackOthers = document.getElementById("techstack-others");
+
+// const HIDDEN = "hidden";
+
+// function handleBackendToggle() {
+//   console.log("clicked");
+//   techstackBackend.toggleAttribute(HIDDEN);
+// }
+
+// function handleFrontendToggle() {
+//   techstackFrontend.toggleAttribute(HIDDEN);
+// }
+
+// function handleOthersToggle() {
+//   techstackOthers.toggleAttribute(HIDDEN);
+// }
+
+// backendBtn.addEventListener("toggle", handleBackendToggle);
+// frontendBtn.addEventListener("toggle", handleFrontendToggle);
+// othersBtn.addEventListener("toggle", handleOthersToggle);
