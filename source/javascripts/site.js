d76825faa9f429fa3773ebdc44cd19526fdea8e4
Yoana Chang <yewonyoana@gmail.com>
2024-06-25 07:22:44 +0900

    Updated Techstack CSS

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -14,30 +14,3 @@ function handleMouseLeave(event) {
 
 aboutDetail.addEventListener("mouseover", handleMouseOver);
 aboutDetail.addEventListener("mouseleave", handleMouseLeave);
-
-// Tech stack click event
-// const backendBtn = document.getElementById("btn-backend");
-// const frontendBtn = document.getElementById("btn-frontend");
-// const othersBtn = document.getElementById("btn-others");
-// const techstackBackend = document.getElementById("techstack-backend");
-// const techstackFrontend = document.getElementById("techstack-frontend");
-// const techstackOthers = document.getElementById("techstack-others");
-
-// const HIDDEN = "hidden";
-
-// function handleBackendToggle() {
-//   console.log("clicked");
-//   techstackBackend.toggleAttribute(HIDDEN);
-// }
-
-// function handleFrontendToggle() {
-//   techstackFrontend.toggleAttribute(HIDDEN);
-// }
-
-// function handleOthersToggle() {
-//   techstackOthers.toggleAttribute(HIDDEN);
-// }
-
-// backendBtn.addEventListener("toggle", handleBackendToggle);
-// frontendBtn.addEventListener("toggle", handleFrontendToggle);
-// othersBtn.addEventListener("toggle", handleOthersToggle);
